59ba3d714230b57a946dc1c577e61b2d6620d1bc
whitehat jr <71430109+raghav1234happy@users.noreply.github.com>
2021-05-02 13:41:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+Webcam.set({
+    width: 310,
+    height: 300,
+    image_format: 'png',
+    png_quality: 90,
+
+    constraints: {
+        facingMode: "environment"
+    }
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+function take_snapshot() {
+    Webcam.snap(function (data_uri) {
+        document.getElementById("result").innerHTML = '<img id="captured_image" src="' + data_uri + '"/>';
+    });
+}
+
+console.log('ml5 version:', ml5.version);
+classifier = ml5.imageClassifier('MobileNet', modalloaded);
+
+function modalloaded() {
+    console.log("Modal is loaded");
+}
+
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotresult);
+}
+
+function gotresult(error, results) {
+    if (error) { console.error(error); }
+    else {
+        console.log(results);
+        document.getElementById("object_name").innerHTML = results[0].label;
+    }
+}
